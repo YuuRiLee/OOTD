1989f10de6da2838d7341dc8a6a0c67b30953935
YuuRiLee <yurilee9696@gmail.com>
2023-06-09 00:52:39 +0900

    feat: 실제 데이터 사용하도록 변경

--- main.py
+++ main.py
@@ -10,26 +10,9 @@ soogsil = {
 }
 
 if __name__ == "__main__":
-    forecast.forecast(
+    data = forecast.forecast(
         soogsil["latitude"],
         soogsil["longitude"]
     )
 
-class Condition(Enum):
-    Sunny = 0
-    Clouds = 1
-    Rain = 2
-    
-def convertData(data):
-    for item in data:
-        # 날짜를 문자열에서 datetime.date 객체로 변환
-        dateStr = item["date"].replace("datetime.date(", "").replace(")", "")
-        year, month, day = map(int, dateStr.split(", "))
-        item["date"] = datetime.date(year, month, day)
-
-    return data
-
-with open('mock_domain_data.json', 'r') as jsonFile:
-    data = json.load(jsonFile)
-    data = convertData(data)
-    interface.createCanvas(data)
+interface.createCanvas(data)
